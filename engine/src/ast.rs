6d7abf1e44a0bbaa5774811e25dcb996fa1a9143
Alexandre Leconte <aleconte@dwightstudio.fr>
2025-11-07 11:27:29 +0100

    Removed test

--- engine/src/ast.rs
+++ engine/src/ast.rs
@@ -1,20 +1,2 @@
 // Include generated sources
 include!(concat!(env!("OUT_DIR"), "/ast.rs"));
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const sample: &str = include_str!("../../examples/variant_1/game.json");
-
-    #[test]
-    fn test_sample() {
-        println!(
-            "{:?}",
-            match serde_json::from_str::<Model>(sample) {
-                Ok(ast) => ast,
-                Err(e) => panic!("{}", e),
-            }
-        );
-    }
-}
